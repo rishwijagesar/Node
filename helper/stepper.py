050f11f5a1811d14e5819384b6034dd6408e733f
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 20:04:02 +0100

    python code

--- helper/stepper.py
+++ helper/stepper.py
@@ -1,56 +1,83 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+# libraries
 import time
+import RPi.GPIO as GPIO
+
+# Use BCM GPIO references
+# Instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
-ControlPin = [7,11,13,15]
 
-for pin in ControlPin:
-	GPIO.setup(pin,GPIO.OUT)
-	GPIO.output(pin,False)
+# Define GPIO signals to use Pins 18,22,24,26 GPIO24,GPIO25,GPIO8,GPIO7
+StepPins = [7,11,13,15]
+# Set all pins as output
+for pin in StepPins:
+        print("Setup pins")
+        GPIO.setup(pin,GPIO.OUT)
+        GPIO.output(pin, False)
+# Define some settings
+WaitTime = 0.005
 
-step_seq_num = 0
-rot_spd = 0.001
+# Define simple sequence
+StepCount1 = 4
+Seq1 = []
+Seq1 = [i for i in range(0, StepCount1)]
+Seq1[0] = [1,0,0,0]
+Seq1[1] = [0,1,0,0]
+Seq1[2] = [0,0,1,0]
+Seq1[3] = [0,0,0,1]
+# Define advanced half-step sequence
+StepCount2 = 8
+Seq2 = []
+Seq2 = [i for i in range(0, StepCount2)]
+Seq2[0] = [1,0,0,0]
+Seq2[1] = [1,1,0,0]
+Seq2[2] = [0,1,0,0]
+Seq2[3] = [0,1,1,0]
+Seq2[4] = [0,0,1,0]
+Seq2[5] = [0,0,1,1]
+Seq2[6] = [0,0,0,1]
+Seq2[7] = [1,0,0,1]
+# Choose a sequence to use
+Seq = Seq2
+StepCount = StepCount2
 
-rotate = 4096
-rotate_dir = 1
+def steps(nb):
+        StepCounter = 0
+        if nb<0: sign=-1
+        else: sign=1
+        nb=sign*nb*2 #times 2 because half-step
+        print("nbsteps {} and sign {}".format(nb,sign))
+        for i in range(nb):
+                for pin in range(4):
+                        xpin = StepPins[pin]
+                        if Seq[StepCounter][pin]!=0:
+                                GPIO.output(xpin, True)
+                        else:
+                                GPIO.output(xpin, False)
+                StepCounter += sign
+        # If we reach the end of the sequence
+        # start again
+                if (StepCounter==StepCount):
+                        StepCounter = 0
+                if (StepCounter<0):
+                        StepCounter = StepCount-1
+                # Wait before moving on
+                time.sleep(WaitTime)
 
-seq = [ [1,0,0,0],
-[1,1,0,0],
-[0,1,0,0],
-[0,1,1,0],
-[0,0,1,0],
-[0,0,1,1],
-[0,0,0,1],
-[1,0,0,1],
-]
+# Start main loop
+nbStepsPerRev=2048
+if __name__ == '__main__' :
+    hasRun=False
+    while not hasRun:
+            steps(nbStepsPerRev)# parcourt un tour dans le sens horaire
+            time.sleep(1)
+            steps(-nbStepsPerRev)# parcourt un tour dans le sens anti-horaire
+            time.sleep(1)
 
-rotateF = float(input("Enter revolutions(0.00041 +):"))
-rotate_dir = input("Enter direction (1CW/-1CW) :")
-rot_spd = input("Enter speed (!-0.001):")
-rotate = int(rotateF * 4096)
-if rotate < 1:
-	rotate = 4096
-	rotate_dir = int(rotate_dir)
-if rotate_dir != 1 and rotate_dir != -1:
-	rotate_dir = 1
-	rot_spd = float(rot_spd)
-if rot_spd > 1 or rot_spd < 0.001:
-	rot_spd = 0.001
-	print(rotate,rotate_dir,rot_spd)
+            hasRun=True
 
-for i in  range(0,(rotate+1)):
-	for pin in range(0,4):
-		pattern_pin = ControlPin[pin]
-		if seq[step_seq_num][pin] == 1:
-			GPIO.output(pattern_pin, True)
-		else:
-			GPIO.output(pattern_pin,False)
-	
-	step_seq_num += rotate_dir
-	if(step_seq_num >= 8):
-		step_seq_num = 0
-	elif step_seq_num <0:
-		step_seq_num = 7
-	
-	time.sleep(rot_spd)
-
-GPIO.cleanup()
+    print("Stop motor")
+    for pin in StepPins:
+            GPIO.output(pin, False)
