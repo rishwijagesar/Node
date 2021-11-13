b842d51865dc8c925aba21f46857cf240af085d2
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 17:23:22 +0100

    add servo

--- routes/servoRoute.js
+++ routes/servoRoute.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const {Board, Servo} = require("johnny-five");
+const { Board, Servo } = require("johnny-five");
 const board = new Board();
 
 
 
-board.on('ready', function() {
- 
+board.on('ready', function () {
 
-    router.get('/spin', function(req, res){
-        const servo  = new Servo.Continuous("P1-11");
+    router.get('/spin', function (req, res) {
+        const servo = new Servo.Continuous("P1-11");
         servo.sweep();
     });
 
-    router.get('/stop', function(req, res){
-        const servo  = new Servo.Continuous("P1-11");
+    router.get('/stop', function (req, res) {
+        const servo = new Servo.Continuous("P1-11");
         servo.stop();
     });
-
-    
 
 });
 
 board.on("exit", () => {
-   servo.stop();
+    const servo = new Servo.Continuous("P1-11");
+    servo.stop();
 });
 
 module.exports = router;
