d964882c8df61d9f58e6f84c92589042f6547da2
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 19:34:16 +0100

    add python script

--- helper/stepper.py
+++ helper/stepper.py
@@ -1,23 +1,44 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
-control_pins = [7,11,13,15]
-for pin in control_pins:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
-halfstep_seq = [
-  [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
-  [0,1,1,0],
-  [0,0,1,0],
-  [0,0,1,1],
-  [0,0,0,1],
-  [1,0,0,1]
-]
-for i in range(512):
-  for halfstep in range(8):
-    for pin in range(4):
-      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-    time.sleep(0.001)
-GPIO.cleanup()
+
+P_A1 = 7  # adapt to your wiring
+P_A2 = 11 # ditto
+P_B1 = 13 # ditto
+P_B2 = 15 # ditto
+delay = 0.005 # time to settle
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(P_A1, GPIO.OUT)
+    GPIO.setup(P_A2, GPIO.OUT)
+    GPIO.setup(P_B1, GPIO.OUT)
+    GPIO.setup(P_B2, GPIO.OUT)
+
+def forwardStep():
+    setStepper(1, 0, 1, 0)
+    setStepper(0, 1, 1, 0)
+    setStepper(0, 1, 0, 1)
+    setStepper(1, 0, 0, 1)
+
+def backwardStep():
+    setStepper(1, 0, 0, 1)
+    setStepper(0, 1, 0, 1)
+    setStepper(0, 1, 1, 0)
+    setStepper(1, 0, 1, 0)
+  
+def setStepper(in1, in2, in3, in4):
+    GPIO.output(P_A1, in1)
+    GPIO.output(P_A2, in2)
+    GPIO.output(P_B1, in3)
+    GPIO.output(P_B2, in4)
+    time.sleep(delay)
+
+setup()
+# 512 steps for 360 degrees, adapt to your motor
+while True:
+    print: "forward"
+    for i in range(256):
+        forwardStep() 
+    print: "backward"
+    for i in range(256):
+        backwardStep() 
