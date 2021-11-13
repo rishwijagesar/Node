f4904fae0633970493f7592ecabcd7c6f0350114
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 16:12:00 +0100

    add light moduses

--- routes/ledRoute.js
+++ routes/ledRoute.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-var raspio = require("raspi-io").RaspiIO;
-var five = require("johnny-five");
-var board =new five.Board({
+const raspio = require("raspi-io").RaspiIO;
+const five = require("johnny-five");
+const board =new five.Board({
     io: new raspio()
 });
-var LEDpin = new five.Led("P1-12");
+
+const LEDpin = new five.Led("P1-12");
 
 board.on('ready', function() {
     
-
+    // turn on led
     router.get('/on', function(req, res){
         LEDpin.on();
     });
 
+    // turn of led
     router.get('/off', function(req, res){
         LEDpin.off();
     });
 
+    // pulse led and fade out after 5 seconds
     router.get('/pulse', function(req, res){
         LEDpin.pulse();
+
+        board.wait(5000, () => {
+            LEDpin.fadeOut();
+        });
     });
 
+    // blink led and fade out after 5 seconds
     router.get('/blink', function(req, res){
         LEDpin.blink(500);
 
