3474bd5a6e1ed1ac0a1cb63d3031ba43608648ed
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 20:38:46 +0100

    python code

--- routes/stepperRoute.js
+++ routes/stepperRoute.js
@@ -1,32 +1,18 @@
 const express = require('express');
+const { spawn } = require('child_process');
 const router = express.Router();
-const raspio = require("raspi-io").RaspiIO;
-const { Board, Stepper } = require("johnny-five");
-const board = new Board({
-    io: new raspio()
+
+router.get('/left', (req, res) => {
+    let dataToSend;
+    const python = spawn('python', ['./helper/stepper.py']);
+
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        // send data to browser
+        res.send(dataToSend)
+    });
 });
 
-// const stepper = new Stepper({
-//     type: Stepper.TYPE.FOUR_WIRE,
-//     stepsPerRev: 200,
-//     pins: {
-//       motor1: "P1-12",
-//       motor2: "P1-13",
-//       motor3: "P1-11",
-//       motor4: "P1-15"
-//     }
-//   });
 
-// board.on('ready', function () {
-
-//     router.get('/move', function (req, res) {        
-       
-//     });   
-
-// });
-
-// board.on("exit", () => {
-//     stepper.stop();
-// });
 
 module.exports = router;
