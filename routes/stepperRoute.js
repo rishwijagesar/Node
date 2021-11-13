8a76d7dc85fba03508f37c0d1f68b153412c4993
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 19:24:47 +0100

    add python script

--- routes/stepperRoute.js
+++ routes/stepperRoute.js
@@ -6,27 +6,27 @@ const board = new Board({
     io: new raspio()
 });
 
-const stepper = new Stepper({
-    type: Stepper.TYPE.FOUR_WIRE,
-    stepsPerRev: 200,
-    pins: {
-      motor1: "P1-12",
-      motor2: "P1-13",
-      motor3: "P1-11",
-      motor4: "P1-15"
-    }
-  });
+// const stepper = new Stepper({
+//     type: Stepper.TYPE.FOUR_WIRE,
+//     stepsPerRev: 200,
+//     pins: {
+//       motor1: "P1-12",
+//       motor2: "P1-13",
+//       motor3: "P1-11",
+//       motor4: "P1-15"
+//     }
+//   });
 
-board.on('ready', function () {
+// board.on('ready', function () {
 
-    router.get('/move', function (req, res) {        
-        stepper.rpm(180).direction(Stepper.DIRECTION.CCW).accel(1600).decel(1600);
-    });   
+//     router.get('/move', function (req, res) {        
+       
+//     });   
 
-});
+// });
 
-board.on("exit", () => {
-    stepper.stop();
-});
+// board.on("exit", () => {
+//     stepper.stop();
+// });
 
 module.exports = router;
