5e52a01772007e5ef03a6313c02b0b84c92848e8
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 20:42:45 +0100

    python code

--- routes/stepperRoute.js
+++ routes/stepperRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/left', (req, res) => {
     let dataToSend;
-    const python = spawn('python', ['./helper/stepper.py']);
+    const python = spawn('python', ['./helper/stepperLeft.py']);
 
     python.on('close', (code) => {
         console.log(`child process close all stdio with code ${code}`);
@@ -15,7 +15,7 @@ router.get('/left', (req, res) => {
 
 router.get('/right', (req, res) => {
     let dataToSend;
-    const python = spawn('python', ['./helper/stepper.py']);
+    const python = spawn('python', ['./helper/stepperRight.py']);
 
     python.on('close', (code) => {
         console.log(`child process close all stdio with code ${code}`);
