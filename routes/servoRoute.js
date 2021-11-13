928a09e9bd20044dc1944660ba5454a0ddb23a77
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 17:10:10 +0100

    add servo

--- routes/servoRoute.js
+++ routes/servoRoute.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const raspio = require("raspi-io").RaspiIO;
 const {five, Servo} = require("johnny-five");
-const board = new five.Board({
-    io: new raspio()
-});
+// const board = new five.Board({
+//     io: new raspio()
+// });
 
 const Servopin = new Servo.Continuous("P1-11");
 
