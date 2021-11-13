0ddc1b899e85f1415069500511d83fd6826c1df3
Rishwi <rishwijagesar@hotmail.com>
2021-11-13 20:18:22 +0100

    python code

--- helper/stepper.py
+++ helper/stepper.py
@@ -1,45 +1,41 @@
 import RPi.GPIO as GPIO
-from time import sleep
-import sys
+import time 
 
-#assign GPIO pins for motor
-motor_channel = (7,11,13,15)  
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-#for defining more than 1 GPIO channel as input/output use
-GPIO.setup(motor_channel, GPIO.OUT)
+motorPins = (12, 16, 18, 22)
+CCWStep = (0x01,0x02,0x04,0x08)
+CWStep = (0x08,0x04,0x02,0x01)
 
-motor_direction = input('select motor direction a=anticlockwise, c=clockwise: ')
-while True:
+def setup():
+    print ('Program is starting...')
+    GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+    for pin in motorPins:
+        GPIO.setup(pin,GPIO.OUT)
+
+def moveOnePeriod(direction,ms):    
+    for j in range(0,4,1):      #cycle for power supply order
+        for i in range(0,4,1):  #assign to each pin, a total of 4 pins
+            if (direction == 1):#power supply order clockwise
+                GPIO.output(motorPins[i],((CCWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
+            else :              #power supply order anticlockwise
+                GPIO.output(motorPins[i],((CWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
+        if(ms<3):       #the delay can not be less than 3ms, otherwise it will exceed speed limit of the motor
+            ms = 3
+        time.sleep(ms*0.001)    
+
+def moveSteps(direction, ms, steps):
+    for i in range(steps):
+        moveOnePeriod(direction, ms)
+
+def motorStop():
+    for i in range(0,4,1):
+        GPIO.output(motorPins[i],GPIO.LOW)
+
+def destroy():
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
     try:
-        if(motor_direction == 'c'):
-            print('motor running clockwise\n')
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
-            sleep(0.02)
-
-        elif(motor_direction == 'a'):
-            print('motor running anti-clockwise\n')
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
-            sleep(0.02)
-
-            
-    #press ctrl+c for keyboard interrupt
+        loop()
     except KeyboardInterrupt:
-        #query for setting motor direction or exit
-        motor_direction = input('select motor direction a=anticlockwise, c=clockwise or q=exit: ')
-        #check for exit
-        if(motor_direction == 'q'):
-            print('motor stopped')
-            sys.exit(0)
+        destroy()
